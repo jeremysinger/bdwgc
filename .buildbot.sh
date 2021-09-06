7b064bbe77b13472a5bc4ac58433e054b1a27367
Dejice Jacob <dejice.jacob@glasgow.ac.uk>
2021-09-06 18:16:53 +0100

    build within source tree

--- .buildbot.sh
+++ .buildbot.sh
@@ -2,16 +2,14 @@
 # vim: expandtab sts=0 sw=4 smarttab
 set -e
 
-ROOT_DIR=$(pwd)/..
-
 # arg-1 : Root directory
 # arg-2 : cmake toolchain file
-function build_boehmGC()
+function build_bdwgc()
 {
     ARCH=$(echo ${2} | cut -d '.' -f 1)
-    SRC_DIR=${1}/build # build/source directory name used for all repos
-    BUILD_DIR=${1}/bdwgc-build
-    INSTALL_DIR=${1}/install
+    SRC_DIR=${1}
+    BUILD_DIR=${SRC_DIR}/bdwgc_build
+    INSTALL_DIR=${SRC_DIR}/bdwgc_install
     BUILD_OPTS="-DCMAKE_BUILD_TYPE=Debug \
                 -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
                 -Denable_gcj_support=OFF \
@@ -26,7 +24,7 @@ function build_boehmGC()
 }
 
 
-build_boehmGC ${ROOT_DIR} riscv64-purecap.cmake
+build_bdwgc $(pwd) riscv64-purecap.cmake
 
 # Clean up build directories
-rm -rf ${ROOT_DIR}/bdwgc-build
+rm -rf $(pwd)/bdwgc_build $(pwd)/bdwgc_install
