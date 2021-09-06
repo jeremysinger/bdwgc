6310360638af7f7162f8888e4d152b64eaa1d6d2
Dejice Jacob <dejice.jacob@glasgow.ac.uk>
2021-09-06 19:14:13 +0100

    Make current directory default source dir

--- .buildbot.sh
+++ .buildbot.sh
@@ -2,14 +2,11 @@
 # vim: expandtab sts=0 sw=4 smarttab
 set -e
 
-# arg-1 : Root directory
-# arg-2 : cmake toolchain file
+# arg-1 : cmake toolchain file
 function build_bdwgc()
 {
-    ARCH=$(echo ${2} | cut -d '.' -f 1)
-    SRC_DIR=${1}
-    BUILD_DIR=${SRC_DIR}/bdwgc_build
-    INSTALL_DIR=${SRC_DIR}/bdwgc_install
+    BUILD_DIR=bdwgc_build
+    INSTALL_DIR=bdwgc_install
     BUILD_OPTS="-DCMAKE_BUILD_TYPE=Debug \
                 -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
                 -Denable_gcj_support=OFF \
@@ -19,12 +16,8 @@ function build_bdwgc()
 
 
     mkdir -p ${BUILD_DIR}
-    cmake -B ${BUILD_DIR} -S ${SRC_DIR} ${BUILD_OPTS} -DCMAKE_TOOLCHAIN_FILE=${2}
+    cmake -B ${BUILD_DIR} -S . ${BUILD_OPTS} -DCMAKE_TOOLCHAIN_FILE=${1}
     cmake --build ${BUILD_DIR}
 }
 
-
-build_bdwgc $(pwd) riscv64-purecap.cmake
-
-# Clean up build directories
-rm -rf $(pwd)/bdwgc_build $(pwd)/bdwgc_install
+build_bdwgc riscv64-purecap.cmake
